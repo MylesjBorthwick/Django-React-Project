00a156ed70d89c35cfa8dd79e0f941570f7baf12
Myles Borthwick <64812046+MylesjBorthwick@users.noreply.github.com>
2020-12-17 19:36:23 -0700

    added counter to courseapp

--- MileStone-1/course-app/src/App.js
+++ MileStone-1/course-app/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'bulma/css/bulma.css';
+import {useState} from "react";
 function App() {
+  
+  const  [counter, setCounter] = useState(0);
+  const  [inputValue, setInputValue] = useState("");
+
+  const decrement = ()=>{setCounter(counter-1)}
+  const increment = ()=>{setCounter(counter+1)}
+
+  const handleKeyDown = (e) => {
+    if(e.key == "Enter"){
+      setCounter(parseInt(inputValue))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <div claddName="container">
+      <div className="columns is multiline">
+        <div className="column is-full">
+          <div className="notification">
+            <div className="columns">
+              <div className="column is-half">
+                <div className="field has-addons">
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Enter Number" 
+                      value = {inputValue}
+                      onChange = {(e)=>setInputValue(e.target.value)}
+                      onKeyDown = {handleKeyDown}
+                      />
+                  </div>
+                  <div class="control">
+                    <a class="button is-info"
+                    onClick = {()=> setCounter(parseInt(inputValue))}
+                    >Set</a>
+                  </div>
+                </div>
+                <div class="buttons has-addons">
+                  <button className="button is-primary"
+                  onClick = {()=>setCounter(increment)}
+                  
+                  >Up</button>
+                  <button className="button is-warning"
+                  onClick = {()=>setCounter(decrement)}
+                  
+                  >Down</button>
+                </div>
+              </div>
+              <div className="column is-half">
+                <h1 className = "title">{counter}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+
+  </div>);
 }
 
 export default App;
