8aed43cc397b30bd7b785dff64215eed17a84090
kenloughery <64917859+kenloughery@users.noreply.github.com>
2020-12-18 04:13:27 -0700

    Removed old irrelevant code

--- MileStone-1/course-app/src/App.js
+++ MileStone-1/course-app/src/App.js
@@ -333,51 +333,6 @@ const refreshPage = () => {
 function App() {
 
 
-  const [employees, setEmployees] = React.useState([])
-
-  React.useEffect(() => {
-      getData()
-  }, [])
-
-  const getData = async () => {
-
-      const response = await axios.get(URL)
-      setEmployees(response.data)
-  }
-
-  const removeData = (id) => {
-
-      axios.delete(`${URL}/${id}`).then(res => {
-          const del = employees.filter(employee => id !== employee.id)
-          setEmployees(del)
-      })
-  }
-
-  const renderHeader = () => {
-      let headerElement = ['id', 'name', 'email', 'phone', 'operation']
-
-      return headerElement.map((key, index) => {
-          return <th key={index}>{key.toUpperCase()}</th>
-      })
-  }
-
-  const renderBody = () => {
-      return employees && employees.map(({ id, name, email, phone }) => {
-          return (
-              <tr key={id}>
-                  <td>{id}</td>
-                  <td>{name}</td>
-                  <td>{email}</td>
-                  <td>{phone}</td>
-                  <td className='opration'>
-                      <button className='button' onClick={() => removeData(id)}>Delete</button>
-                  </td>
-              </tr>
-          )
-      })
-  }
-
-
   return (
     <div className="App">
       <div className="container">
